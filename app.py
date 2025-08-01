48b3f69950e1a7ae197f767c7bd36ef948d2a28f
MSVWalker <michaelsvwalker@gmail.com>
2025-07-31 20:28:49 -0700

    Updated dailyy avg temp .py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from src.plots.anomalies_scatter import plot_anomaly_scatter
 from src.plots.top10_bar import plot_top10_hottest_years
 from src.plots.record_calendar import plot_record_calendar
 from src.plots.monthly_avg_temp import plot_monthly_avg_temp
-from src.plots.daily_avg_temp import plot_daily_avg_temp
+from src.plots.daily_avg_temp import plot_yearly_avg_temp
 
 # --- Setup ---
 st.set_page_config(page_title="Bellingham Weather Dashboard", layout="wide")
@@ -29,7 +29,7 @@ st.header("📈 Weather Visualizations")
 figures = []
 
 # Add custom visualizations
-figures.append(plot_daily_avg_temp(df))
+figures.append(plot_yearly_avg_temp(df))
 figures.append(plot_monthly_avg_temp(df))
 figures.append(plot_over_80_streaks(df))
 figures.append(plot_anomaly_scatter(df))
